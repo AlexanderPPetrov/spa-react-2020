af414c29435e662cc2e80caa12c3987ea1fdfbc5
Ivan Zhelev <nvjcompany@gmail.com>
2020-01-25 16:49:09 +0200

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import QuantityInput from '../components/QuantityInput';
+
 class Home extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            counts: [1,2,3,4,5],
+        }
+    }
+    getQuantityInputs = () => {
+        const counts = this.state.counts;
+
+        const inputs = counts.map(element => {
+            return <QuantityInput 
+                key={element} 
+                count={element}/>
+        })
+
+        return inputs;
+    }
+    changeCounts = () => {
+        this.setState({
+            counts: [7, 2]
+        })
+    }
     render() {
         return <div className="col-md-3">
-            <QuantityInput count={5}/>
+                <button
+                    onClick={this.changeCounts} 
+                    type="button" 
+                    className="btn btn-primary"
+                >Change Counts</button>
+            {this.getQuantityInputs()}
         </div>
     }
 }
