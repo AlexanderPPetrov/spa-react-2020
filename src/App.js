0693c95ed5fafe7daf1da44bc40667225267dd82
Ivan Zhelev <nvjcompany@gmail.com>
2020-01-25 12:22:54 +0200

    added router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Users extends React.Component {
+
+
+    render() {
+        return <div>
+            Users Page
+        </div>
+    }
+}
+
+export default Users;
